4b0be45f6f7a25610e3d8d86cefdfca9ca718933
a7987083 <85878547+a7987083@users.noreply.github.com>
2026-09-19 16:23:42 +0800

    zpatchig v0.3: add legacy offset resolver interface

---
+++ hfamap/src/ZPLegacyOffsetResolver.h
@@ -0,0 +1,7 @@
+#pragma once
+#ifdef __OBJC__
+#import <Foundation/Foundation.h>
+NS_ASSUME_NONNULL_BEGIN
+void ZPLegacyOffsetResolverSchedule(id wrapper, NSString *identifier, NSDictionary * _Nullable feature, NSString *expectedImage);
+NS_ASSUME_NONNULL_END
+#endif
