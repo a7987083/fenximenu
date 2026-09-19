bb48107e668860fa91c5bb1430f1f7b961c60353
a7987083 <85878547+a7987083@users.noreply.github.com>
2026-09-19 18:31:03 +0800

    zpatchig v0.4: allow primary container feature merge

--- hfamap/src/ZPFeatureResolver.h
+++ hfamap/src/ZPFeatureResolver.h
@@ -4,6 +4,7 @@
 NS_ASSUME_NONNULL_BEGIN
 void ZPFeatureResolverReset(void);
 NSArray<NSDictionary *> *ZPFeatureResolverCollect(void);
+void ZPFeatureResolverMergeContainerFeatures(NSArray<NSDictionary *> *features);
 NSDictionary * _Nullable ZPFeatureForIdentifier(NSString *identifier);
 NS_ASSUME_NONNULL_END
 #endif
