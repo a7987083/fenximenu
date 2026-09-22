5ba768fd936ec7a763ec759c60012852d210e071
a7987083 <85878547+a7987083@users.noreply.github.com>
2026-09-23 00:13:17 +0800

    feat(hfamap): add stripped action analyzer interface

---
+++ hfamap/src/HFAMapStrippedActionAnalyzer.h
@@ -0,0 +1,4 @@
+#import <Foundation/Foundation.h>
+
+FOUNDATION_EXPORT NSDictionary *HFAMapAnalyzeStrippedActionIMP(const void *implementation,
+                                                                NSString *implementationPath);
