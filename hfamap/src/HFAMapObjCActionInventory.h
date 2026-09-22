6ec0c50a91f8aab4a6450a763e64414cdd7bd5c3
a7987083 <85878547+a7987083@users.noreply.github.com>
2026-09-23 00:14:49 +0800

    feat(hfamap): add stripped objc action inventory interface

---
+++ hfamap/src/HFAMapObjCActionInventory.h
@@ -0,0 +1,4 @@
+#import <Foundation/Foundation.h>
+
+FOUNDATION_EXPORT NSDictionary *HFAMapInventoryStrippedObjCActions(NSString *implementationPath,
+                                                                    NSTimeInterval deadline);
