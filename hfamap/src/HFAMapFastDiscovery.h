472f40bbb7eb458b608289b20eb28ae40cfa27d5
a7987083 <85878547+a7987083@users.noreply.github.com>
2026-09-23 06:25:48 +0800

    feat(hfamap): add independent fast menu discovery interface

---
+++ hfamap/src/HFAMapFastDiscovery.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#ifdef __OBJC__
+#import <Foundation/Foundation.h>
+NS_ASSUME_NONNULL_BEGIN
+NSArray<NSDictionary *> *HFAMapFastDiscoverMenuImages(NSMutableArray<NSDictionary *> *events);
+NS_ASSUME_NONNULL_END
+#endif
