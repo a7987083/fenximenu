cb810cf16e30f383927adb5b90988162b4270179
a7987083 <85878547+a7987083@users.noreply.github.com>
2026-09-23 15:03:06 +0800

    chore(hfamap): advance diagnostics to v2.5.1

--- hfamap/src/HFAMapDiagnostics.mm
+++ hfamap/src/HFAMapDiagnostics.mm
@@ -36,7 +36,7 @@ NSString *HFADiagnosticsBeginSession(void) {
     NSString *session = gHFASessionID;
     pthread_mutex_unlock(&gHFADiagnosticsLock);
     HFADiagnosticsLog(@"session", @"start", @{
-        @"version": @"2.5.0-dev-descriptor-callback-method",
+        @"version": @"2.5.1-dev-generic-feature-handler-method-descriptor",
         @"hostAppName": HFAHostAppName(),
         @"process": NSProcessInfo.processInfo.processName ?: @"?",
         @"os": UIDevice.currentDevice.systemVersion ?: @"?"
