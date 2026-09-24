1b51ebf380b343f1169815340b8a19baab114d67
a7987083 <85878547+a7987083@users.noreply.github.com>
2026-09-24 23:28:12 +0800

    feat(hfamap): add v2.5.17 version marker

---
+++ hfamap/src/HFAMapVersion2517.mm
@@ -0,0 +1,4 @@
+#import <Foundation/Foundation.h>
+
+__attribute__((used)) static const char *kHFAMapVersion2517 = "2.5.17-dev-sender-field-callresult";
+__attribute__((used)) static const char *kHFAMapPolicy2517 = "READ-ONLY-SENDER-FIELD-STACK-CALLRESULT-BRANCH-PROVENANCE";
