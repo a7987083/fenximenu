f5af3a7920faa050b457168ad7aeb601d5ea5253
a7987083 <85878547+a7987083@users.noreply.github.com>
2026-09-24 21:00:01 +0800

    feat(hfamap): add v2.5.14 version marker

---
+++ hfamap/src/HFAMapVersion2514.mm
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+
+__attribute__((used)) static NSString * const HFAMapVersion2514 = @"HFAMap v2.5.14-dev";
+__attribute__((used)) static NSString * const HFAMapComponent2514 = @"2.5.14-dev-feature-ownership-correlator";
+__attribute__((used)) static NSString * const HFAMapPolicy2514 = @"EXACT-OBJECT-EDGE-ONLY-NO-NEARBY-BLOCK-PROPAGATION";
