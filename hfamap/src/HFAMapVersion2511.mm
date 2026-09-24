d7a47098481ede391694d61a77728dfefec09525
a7987083 <85878547+a7987083@users.noreply.github.com>
2026-09-24 18:27:39 +0800

    feat(hfamap): add v2.5.11 version marker

---
+++ hfamap/src/HFAMapVersion2511.mm
@@ -0,0 +1,4 @@
+#import <Foundation/Foundation.h>
+
+__attribute__((used)) static const char *kHFAMapVersion2511 = "HFAMap v2.5.11-dev";
+__attribute__((used)) static const char *kHFAMapComponent2511 = "2.5.11-dev-secret-wrapper-static-evidence";
