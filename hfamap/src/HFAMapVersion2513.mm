05196db18e91bb085d45c4353259e393f8ba75d4
a7987083 <85878547+a7987083@users.noreply.github.com>
2026-09-24 19:59:58 +0800

    feat(hfamap): add v2.5.13 version marker

---
+++ hfamap/src/HFAMapVersion2513.mm
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+
+__attribute__((used)) static const char *kHFAMapVersion2513 = "HFAMap v2.5.13-dev";
+__attribute__((used)) static const char *kHFAMapComponent2513 = "2.5.13-dev-reference-feature-enumerator";
+__attribute__((used)) static const char *kHFAMapPolicy2513 = "REFERENCE-INSPIRED-READ-ONLY-UI-PATCH-BLOCK-MULTIPATH";
