d604920909799fbc44ca357f7866583411eb12a5
a7987083 <85878547+a7987083@users.noreply.github.com>
2026-09-24 21:46:10 +0800

    feat(hfamap): add v2.5.16 version marker

---
+++ hfamap/src/HFAMapVersion2516.mm
@@ -0,0 +1,4 @@
+#import <Foundation/Foundation.h>
+
+__attribute__((used)) static NSString * const HFAMapVersion2516 = @"2.5.16-dev-sender-derived-provenance";
+__attribute__((used)) static NSString * const HFAMapVersion2516Policy = @"READ-ONLY-SENDER-FIELD-CALL-BRANCH-PROVENANCE";
