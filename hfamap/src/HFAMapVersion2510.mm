6b85c686b01297bc5bd57ccea883ff4dca412304
a7987083 <85878547+a7987083@users.noreply.github.com>
2026-09-24 18:05:44 +0800

    feat(hfamap): add v2.5.10 version marker

---
+++ hfamap/src/HFAMapVersion2510.mm
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+
+__attribute__((constructor)) static void HFAMapVersion2510Marker(void) {
+    NSLog(@"[HFAMap] HFAMap v2.5.10-dev directed descriptor resolution");
+}
