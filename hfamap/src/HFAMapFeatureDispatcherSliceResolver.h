7e4749ff6f6c5f6a222bd7f3452b6c13471d1d15
a7987083 <85878547+a7987083@users.noreply.github.com>
2026-09-25 13:19:55 +0800

    feat(hfamap): add feature dispatcher slice resolver interface

---
+++ hfamap/src/HFAMapFeatureDispatcherSliceResolver.h
@@ -0,0 +1,4 @@
+#import <Foundation/Foundation.h>
+
+NSDictionary *HFAMapBuildFeatureDispatcherSlices(NSDictionary *featureBranchProvenance, NSError **error);
+BOOL HFAMapPersistFeatureDispatcherSlices(NSDictionary *result, NSError **error);
