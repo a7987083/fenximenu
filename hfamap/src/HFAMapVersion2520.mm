c2089cd39d75e68ba81f137dc5900f163cd12f55
a7987083 <85878547+a7987083@users.noreply.github.com>
2026-09-25 11:48:37 +0800

    feat(hfamap): add v2.5.20 menu implementation version marker

---
+++ hfamap/src/HFAMapVersion2520.mm
@@ -0,0 +1,4 @@
+#import <Foundation/Foundation.h>
+
+__attribute__((used)) static NSString *const kHFAMapVersion2520 = @"2.5.20-dev-menu-implementation-inventory";
+__attribute__((used)) static NSString *const kHFAMapPolicy2520 = @"READ-ONLY-UNIFIED-PATCH-RUNTIME-METHOD-STATE-MUTATION-EVIDENCE";
