a6011a55ac6e67b8b8da834d67349388730f2c6c
a7987083 <85878547+a7987083@users.noreply.github.com>
2026-09-25 12:24:06 +0800

    feat(hfamap): add v2.5.20.1 cache hotfix marker

---
+++ hfamap/src/HFAMapVersion25201.mm
@@ -0,0 +1,4 @@
+#import <Foundation/Foundation.h>
+
+__attribute__((used)) static NSString * const kHFAMapVersion25201 = @"2.5.20.1-dev-inventory-cache-hotfix";
+__attribute__((used)) static NSString * const kHFAMapPolicy25201 = @"READ-ONLY-BOUNDED-SHARED-IMPLEMENTATION-CACHE";
