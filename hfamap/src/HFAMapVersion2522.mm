58d6f5f7a2c27617bf9f12a844a1e51ccf177b8e
a7987083 <85878547+a7987083@users.noreply.github.com>
2026-09-25 13:20:50 +0800

    chore(hfamap): add v2.5.22 feature slicing marker

---
+++ hfamap/src/HFAMapVersion2522.mm
@@ -0,0 +1,4 @@
+#import <Foundation/Foundation.h>
+
+NSString * const HFAMapVersion2522 = @"2.5.22-dev-feature-dispatcher-slicing";
+NSString * const HFAMapPolicy2522 = @"READ-ONLY-ENTRY-SENDER-FEATURE-SLICING-NO-REEXECUTION";
