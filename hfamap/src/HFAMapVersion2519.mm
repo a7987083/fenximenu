e0931ebd8fe8b967d7ab85641367c8e405ddea16
a7987083 <85878547+a7987083@users.noreply.github.com>
2026-09-25 10:48:58 +0800

    feat(hfamap): add v2.5.19 stability version marker

---
+++ hfamap/src/HFAMapVersion2519.mm
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+
+__attribute__((used)) static NSString *const kHFAMapVersion2519 = @"2.5.19-dev-stability-verified-helper-return";
+__attribute__((used)) static NSString *const kHFAMapPolicy2519 = @"READ-ONLY-FAIL-CLOSED-VERIFIED-HELPER-RETURN-PROVENANCE";
+__attribute__((used)) static NSString *const kHFAMapLoadedPolicy2519 = @"DISABLED-UNTRUSTED-GLOBAL-POINTER-FAIL-CLOSED";
