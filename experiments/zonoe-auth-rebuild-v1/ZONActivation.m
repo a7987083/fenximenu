de1e1992e0b5cd414453a89f52ce3e8e700f6a95
a7987083 <85878547+a7987083@users.noreply.github.com>
2026-09-27 01:46:54 +0800

    auth-flow: make activation response server-driven

--- experiments/zonoe-auth-rebuild-v1/ZONActivation.m
+++ experiments/zonoe-auth-rebuild-v1/ZONActivation.m
@@ -11,14 +11,21 @@
         NSDictionary *d=[json isKindOfClass:NSDictionary.class]?json:@{};
         NSString *rawText=data.length?[[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding]:@"";
         id msgObj=d[@"message"]?:d[@"msg"];
-        NSString *msg=[msgObj isKindOfClass:NSString.class]?msgObj:(rawText.length?rawText:@"服务器未返回消息");
+        NSString *msg=[msgObj isKindOfClass:NSString.class]?msgObj:(rawText.length?rawText:@"");
         BOOL ok=NO;
-        if([d[@"ok"] respondsToSelector:@selector(boolValue)]) ok=[d[@"ok"] boolValue];
-        NSString *code=[d[@"code"] isKindOfClass:NSString.class]?[d[@"code"] lowercaseString]:@"";
-        if([code isEqualToString:@"ok"]||[code isEqualToString:@"success"]||[code isEqualToString:@"activated"]||[code isEqualToString:@"bound"]) ok=YES;
-        if(!d[@"ok"] && !code.length && ([msg containsString:@"成功"]||[msg.lowercaseString isEqualToString:@"ok"])) ok=YES;
-        NSDictionary *display=d.count?d:@{@"message":msg?:@""};
-        if(ok){ [ZONStorage setCard:card]; [ZONStorage setLastActivationObject:display]; [ZONStorage setJustActivated:YES]; }
+        if([d[@"ok"] respondsToSelector:@selector(boolValue)]) {
+            ok=[d[@"ok"] boolValue];
+        } else {
+            NSString *code=[d[@"code"] isKindOfClass:NSString.class]?[d[@"code"] lowercaseString]:@"";
+            ok=[@[@"ok",@"success",@"activated",@"bound"] containsObject:code];
+        }
+        NSDictionary *display=d.count?d:(msg.length?@{@"message":msg}:@{});
+        if(ok){
+            [ZONStorage setUDID:udid];
+            [ZONStorage setCard:card];
+            [ZONStorage setLastActivationObject:display];
+            [ZONStorage setJustActivated:YES];
+        }
         if(completion)completion(ok,msg?:@"",display);
     }];
 }
