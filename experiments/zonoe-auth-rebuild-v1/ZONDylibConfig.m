4d160a4013323a7454d8789c09b3ae0309ad7f51
a7987083 <85878547+a7987083@users.noreply.github.com>
2026-09-26 21:25:46 +0800

    v2: use new verify API config with public placeholder secret

--- experiments/zonoe-auth-rebuild-v1/ZONDylibConfig.m
+++ experiments/zonoe-auth-rebuild-v1/ZONDylibConfig.m
@@ -1,34 +1,12 @@
 #import "ZONDylibConfig.h"
-
 @implementation ZONDylibConfig
-
-+ (ZONDylibVerifyConfiguration *)configurationWithUDIDProvider:(ZONDylibUDIDProvider)udidProvider
-{
-    ZONDylibVerifyConfiguration *cfg = [ZONDylibVerifyConfiguration new];
-    cfg.bootstrapURLs = @[[NSURL URLWithString:@"https://raw.githubusercontent.com/a7987083/zonoemenu-config/main/bootstrap/zonoe.main.json"]];
-    cfg.endpointURL = [NSURL URLWithString:@"https://app3.zonoeios.xyz/index/dylib_verify/verify"];
-    cfg.dylibKey = @"zonoe.main";
-    cfg.dylibVersion = @"1";
-    cfg.dylibBuild = @"";
-    cfg.verifySecret = @"ZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ";
-    cfg.requestTimeout = 10.0;
-    cfg.udidProvider = udidProvider;
-    return cfg;
++ (ZONDylibVerifyConfiguration *)configurationWithUDIDProvider:(ZONDylibUDIDProvider)udidProvider {
+    ZONDylibVerifyConfiguration *cfg=[ZONDylibVerifyConfiguration new];
+    cfg.bootstrapURLs=@[[NSURL URLWithString:@"https://raw.githubusercontent.com/a7987083/zonoemenu-config/main/bootstrap/zonoe.main.json"]];
+    cfg.endpointURL=[NSURL URLWithString:@"https://app3.zonoeios.xyz/index/dylib_verify/verify"];
+    cfg.dylibKey=@"zonoe.main"; cfg.dylibVersion=@"1"; cfg.dylibBuild=@"";
+    cfg.verifySecret=@"ZON_VERIFY_SECRET_PLACEHOLDER_20260926_V2_______________________";
+    cfg.requestTimeout=10.0; cfg.udidProvider=udidProvider; return cfg;
 }
-
-+ (NSArray<NSURL *> *)legacyDylibURLs
-{
-    return @[[NSURL URLWithString:@"https://app3.zonoeios.xyz/index/index/dylib"]];
-}
-
-+ (NSArray<NSURL *> *)legacyApiFaceURLs
-{
-    return @[[NSURL URLWithString:@"https://app3.zonoeios.xyz/index/index/apiface"]];
-}
-
-+ (NSDictionary<NSString *, id> *)metadata
-{
-    return @{@"dylib_id":@(5), @"dylib_key":@"zonoe.main", @"version_id":@(2), @"version_state":@"testing", @"runtime_config_version":@(3), @"default_fail_action":@"disable_feature", @"default_offline_grace":@(900)};
-}
-
++ (NSDictionary<NSString *,id> *)metadata { return @{@"generator_version":@"2.2.0",@"dylib_id":@(5),@"dylib_key":@"zonoe.main",@"version_id":@(2),@"version_state":@"testing",@"runtime_config_version":@(3)}; }
 @end
