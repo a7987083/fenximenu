4c8d49c8801c299fd12534eedfde1eedabf3d042
a7987083 <85878547+a7987083@users.noreply.github.com>
2026-09-26 21:24:39 +0800

    v2: make activation preserve server response

--- experiments/zonoe-auth-rebuild-v1/ZONActivation.h
+++ experiments/zonoe-auth-rebuild-v1/ZONActivation.h
@@ -1,7 +1,12 @@
 #import <Foundation/Foundation.h>
 NS_ASSUME_NONNULL_BEGIN
+@interface ZONActivationResult : NSObject
+@property(nonatomic,assign) BOOL success;
+@property(nonatomic,copy) NSString *message;
+@property(nonatomic,strong) id displayObject;
+@property(nonatomic,copy) NSDictionary *json;
+@end
 @interface ZONActivation : NSObject
-+ (void)activateUDID:(NSString *)udid card:(NSString *)card completion:(void (^)(BOOL success, NSString *message, NSDictionary * _Nullable raw))completion;
-+ (void)fetchLegacyStatusForUDID:(NSString *)udid completion:(void (^)(NSDictionary *status))completion;
++ (void)activateUDID:(NSString *)udid card:(NSString *)card completion:(void (^)(ZONActivationResult *result))completion;
 @end
 NS_ASSUME_NONNULL_END
