ab4fd7252a061b210999425efc59fa625c1fdaad
a7987083 <85878547+a7987083@users.noreply.github.com>
2026-09-26 22:37:56 +0800

    api: expose shared verify secret accessor for apiface HMAC

--- experiments/zonoe-auth-rebuild-v1/ZONDylibConfig.h
+++ experiments/zonoe-auth-rebuild-v1/ZONDylibConfig.h
@@ -4,5 +4,6 @@ NS_ASSUME_NONNULL_BEGIN
 @interface ZONDylibConfig : NSObject
 + (ZONDylibVerifyConfiguration *)configurationWithUDIDProvider:(ZONDylibUDIDProvider)udidProvider;
 + (NSDictionary<NSString *, id> *)metadata;
++ (NSString *)verifySecret;
 @end
 NS_ASSUME_NONNULL_END
