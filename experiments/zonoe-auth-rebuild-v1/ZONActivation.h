4b5936ff7194a925d046cbfca6357859a86df0f1
a7987083 <85878547+a7987083@users.noreply.github.com>
2026-09-26 22:29:52 +0800

    api: make card activation explicitly configuration-driven

--- experiments/zonoe-auth-rebuild-v1/ZONActivation.h
+++ experiments/zonoe-auth-rebuild-v1/ZONActivation.h
@@ -1,9 +1,9 @@
 #import <Foundation/Foundation.h>
 NS_ASSUME_NONNULL_BEGIN
 @interface ZONActivation : NSObject
++ (BOOL)isConfigured;
 + (void)activateUDID:(NSString *)udid
                 card:(NSString *)card
           completion:(void (^)(BOOL ok, NSString *message, NSDictionary * _Nullable raw))completion;
-+ (void)fetchLegacyStatusForUDID:(NSString *)udid completion:(void (^)(NSDictionary *status))completion;
 @end
 NS_ASSUME_NONNULL_END
