ef8945af4f61a1c1a12c6cb96e942abb2bc9d562
a7987083 <85878547+a7987083@users.noreply.github.com>
2026-09-26 17:09:23 +0800

    rebuild: add activation interface

---
+++ experiments/zonoe-auth-rebuild-v1/ZONActivation.h
@@ -0,0 +1,7 @@
+#import <Foundation/Foundation.h>
+NS_ASSUME_NONNULL_BEGIN
+@interface ZONActivation : NSObject
++ (void)activateUDID:(NSString *)udid card:(NSString *)card completion:(void (^)(BOOL success, NSString *message, NSDictionary * _Nullable raw))completion;
++ (void)fetchLegacyStatusForUDID:(NSString *)udid completion:(void (^)(NSDictionary *status))completion;
+@end
+NS_ASSUME_NONNULL_END
