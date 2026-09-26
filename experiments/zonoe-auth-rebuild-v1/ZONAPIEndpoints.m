ffabbcff51539127b9e009bb9dd0ece2f97136ff
a7987083 <85878547+a7987083@users.noreply.github.com>
2026-09-27 00:33:48 +0800

    api-smoke: map documented endpoints

--- experiments/zonoe-auth-rebuild-v1/ZONAPIEndpoints.m
+++ experiments/zonoe-auth-rebuild-v1/ZONAPIEndpoints.m
@@ -4,12 +4,15 @@
 + (NSURL *)authorizationURL { return [NSURL URLWithString:[[self base] stringByAppendingString:@"/authorization"]]; }
 + (NSURL *)activationURLForUDID:(NSString *)udid card:(NSString *)card {
     NSURLComponents *c=[NSURLComponents componentsWithString:[[self base] stringByAppendingString:@"/appstore"]];
-    c.queryItems=@[[NSURLQueryItem queryItemWithName:@"udid" value:udid?:@""],[NSURLQueryItem queryItemWithName:@"code" value:card?:@""]];
+    NSMutableArray *items=[NSMutableArray arrayWithObject:[NSURLQueryItem queryItemWithName:@"udid" value:udid?:@""]];
+    if(card.length) [items addObject:[NSURLQueryItem queryItemWithName:@"code" value:card]];
+    c.queryItems=items;
     return c.URL;
 }
-+ (NSURL *)apiFaceURLForUDID:(NSString *)udid {
-    NSURLComponents *c=[NSURLComponents componentsWithString:[[self base] stringByAppendingString:@"/index/index/apiface"]];
-    c.queryItems=@[[NSURLQueryItem queryItemWithName:@"udid" value:udid?:@""]];
-    return c.URL;
-}
++ (NSURL *)apiFaceURLForUDID:(NSString *)udid { NSURLComponents *c=[NSURLComponents componentsWithString:[[self base] stringByAppendingString:@"/index/index/apiface"]]; c.queryItems=@[[NSURLQueryItem queryItemWithName:@"udid" value:udid?:@""]]; return c.URL; }
++ (NSURL *)legacyDylibConfigURLForUDID:(NSString *)udid { NSURLComponents *c=[NSURLComponents componentsWithString:[[self base] stringByAppendingString:@"/index/index/dylib"]]; c.queryItems=@[[NSURLQueryItem queryItemWithName:@"udid" value:udid?:@""]]; return c.URL; }
++ (NSURL *)unbindQueryURLForUDID:(NSString *)udid { NSURLComponents *c=[NSURLComponents componentsWithString:[[self base] stringByAppendingString:@"/unbind/query"]]; c.queryItems=@[[NSURLQueryItem queryItemWithName:@"udid" value:udid?:@""]]; return c.URL; }
++ (NSURL *)unbindURL { return [NSURL URLWithString:[[self base] stringByAppendingString:@"/unbind"]]; }
++ (NSURL *)runtimeConfigURLForDylibKey:(NSString *)dylibKey { NSURLComponents *c=[NSURLComponents componentsWithString:[[self base] stringByAppendingString:@"/index/dylib_verify/config"]]; c.queryItems=@[[NSURLQueryItem queryItemWithName:@"dylib_key" value:dylibKey?:@""]]; return c.URL; }
++ (NSURL *)verifyURL { return [NSURL URLWithString:[[self base] stringByAppendingString:@"/index/dylib_verify/verify"]]; }
 @end
